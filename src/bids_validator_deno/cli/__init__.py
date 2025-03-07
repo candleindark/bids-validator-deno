d336d1c041cdd6ef6903a7c1062af56230bc8fea
Isaac To <isaac.chun.to@gmail.com>
2025-03-07 10:25:56 -0800

    fix: use return code of `bids_validator_deno` to pass exit status

--- src/bids_validator_deno/cli/__init__.py
+++ src/bids_validator_deno/cli/__init__.py
@@ -8,7 +8,13 @@ from bids_validator_deno.__about__ import __version__
 
 @click.group(context_settings={"help_option_names": ["-h", "--help"]}, invoke_without_command=True)
 @click.version_option(version=__version__, prog_name="bids-validator-deno")
-def bids_validator_deno():
+def bids_validator_deno() -> int:
+    """
+    Python entry point to invoke the BIDS validator binary built with `deno compile`.
+
+    :return: The exit code of the BIDS validator binary.
+    """
+
     import importlib.resources
     import subprocess
     import sys
@@ -19,7 +25,6 @@ def bids_validator_deno():
     try:
         subprocess.run(str(binary_path), check=True)
     except subprocess.CalledProcessError as e:
-        sys.exit(e.returncode)
+        return e.returncode
     else:
-        sys.exit(0)
-
+        return 0
