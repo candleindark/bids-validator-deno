97cc31036b6e798c74989612eeae945ac93618bd
Isaac To <isaac.chun.to@gmail.com>
2025-03-07 10:48:17 -0800

    feat: pass invocation CLI args directly to deno compile binary
    The click dependency is removed because it is no longer
    needed

--- src/bids_validator_deno/cli/__init__.py
+++ src/bids_validator_deno/cli/__init__.py
@@ -1,16 +1,8 @@
 # SPDX-FileCopyrightText: 2025-present Isaac To <candleindark@gmail.com>
 #
 # SPDX-License-Identifier: MIT
-import click
-
-from bids_validator_deno.__about__ import __version__
 
 
-@click.group(
-    context_settings={"help_option_names": ["-h", "--help"]},
-    invoke_without_command=True,
-)
-@click.version_option(version=__version__, prog_name="bids-validator-deno")
 def bids_validator_deno() -> int:
     """
     Python entry point to invoke the BIDS validator binary built with `deno compile`.
@@ -28,7 +20,7 @@ def bids_validator_deno() -> int:
     )
 
     try:
-        subprocess.run(str(binary_path), check=True)
+        subprocess.run([str(binary_path)] + sys.argv[1:], check=True)
     except subprocess.CalledProcessError as e:
         return e.returncode
     else:
