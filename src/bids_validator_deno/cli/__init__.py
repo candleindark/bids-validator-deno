4a29f06616dc150a247b2dc8282c767249350e01
Isaac To <isaac.chun.to@gmail.com>
2025-03-07 10:28:48 -0800

    style: reformat file using `black`

--- src/bids_validator_deno/cli/__init__.py
+++ src/bids_validator_deno/cli/__init__.py
@@ -6,7 +6,10 @@ import click
 from bids_validator_deno.__about__ import __version__
 
 
-@click.group(context_settings={"help_option_names": ["-h", "--help"]}, invoke_without_command=True)
+@click.group(
+    context_settings={"help_option_names": ["-h", "--help"]},
+    invoke_without_command=True,
+)
 @click.version_option(version=__version__, prog_name="bids-validator-deno")
 def bids_validator_deno() -> int:
     """
@@ -20,7 +23,9 @@ def bids_validator_deno() -> int:
     import sys
 
     # Locate the binary within the package
-    binary_path = importlib.resources.files("bids_validator_deno").joinpath("bin", "bids-validator")
+    binary_path = importlib.resources.files("bids_validator_deno").joinpath(
+        "bin", "bids-validator"
+    )
 
     try:
         subprocess.run(str(binary_path), check=True)
