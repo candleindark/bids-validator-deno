137d2f18c376ad9dd1aa19bfd2047ab2711ab85b
Isaac To <isaac.chun.to@gmail.com>
2025-03-06 19:50:21 -0800

    feat: have `bids_validator_deno` command invoke bids-validator binary

--- src/bids_validator_deno/cli/__init__.py
+++ src/bids_validator_deno/cli/__init__.py
@@ -9,4 +9,17 @@ from bids_validator_deno.__about__ import __version__
 @click.group(context_settings={"help_option_names": ["-h", "--help"]}, invoke_without_command=True)
 @click.version_option(version=__version__, prog_name="bids-validator-deno")
 def bids_validator_deno():
-    click.echo("Hello world!")
+    import importlib.resources
+    import subprocess
+    import sys
+
+    # Locate the binary within the package
+    binary_path = importlib.resources.files("bids_validator_deno").joinpath("bin", "bids-validator")
+
+    try:
+        subprocess.run(str(binary_path), check=True)
+    except subprocess.CalledProcessError as e:
+        sys.exit(e.returncode)
+    else:
+        sys.exit(0)
+
