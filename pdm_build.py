f12715d4ca6cfc7ae1c846209945399a2779c467
Yaroslav Halchenko <debian@onerussian.com>
2025-03-10 18:45:03 -0400

    pickup more for pdb

--- pdm_build.py
+++ pdm_build.py
@@ -3,10 +3,7 @@
 import platform
 import sys
 import os
-from packaging.tags import sys_tags
-from typing import Any
-
-from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+from wheel.cli.tags import tags
 
 
 def get_platform_tag():
@@ -38,14 +35,35 @@ def get_platform_tag():
     return f"py3-none-{os_tag}"
 
 
+# picked up from 
+# https://github.com/Bing-su/pip-binary-factory/blob/main/zig/pdm_build.py
 
-class CustomBuildHook(BuildHookInterface):
-    def initialize(self, version: str, build_data: dict[str, Any]) -> None:
-        build_data["pure_python"] = False
-        # build_data["infer_tag"] = True
-        build_data["tag"] = get_platform_tag()
+def pdm_build_hook_enabled(context: Context):
+    return context.target != "sdist"
 
-# Example usage
+
+def pdm_build_initialize(context: Context) -> None:
+    setting = {"--python-tag": "py3", "--py-limited-api": "none"}
+    context.builder.config_settings = {**setting, **context.builder.config_settings}
+
+    context.ensure_build_dir()
+    download(context.build_dir)
+
+
+def pdm_build_finalize(context: Context, artifact: Path) -> None:
+    platform_tags = ZIG_PYTHON_PLATFORMS[get_platform()]
+    renamed = tags(
+        str(artifact),
+        python_tags="py3",
+        abi_tags="none",
+        platform_tags=get_platform_tag(), ## TODO transform
+        remove=True,
+    )
+    print(renamed)
+
+    if context.build_dir.exists():
+        shutil.rmtree(context.build_dir)
+
 if __name__ == "__main__":
     try:
         tag = get_platform_tag()
