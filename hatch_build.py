ae95f916ec603b59785e54ddd581de0ea0ac08f0
Isaac To <isaac.chun.to@gmail.com>
2025-03-10 03:54:03 -0700

    fix: mark wheel build result as platform dependent

---
+++ hatch_build.py
@@ -0,0 +1,9 @@
+from typing import Any
+
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+
+
+class CustomBuildHook(BuildHookInterface):
+    def initialize(self, version: str, build_data: dict[str, Any]) -> None:
+        build_data["pure_python"] = False
+        build_data["infer_tag"] = True
