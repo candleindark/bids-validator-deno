3d5e5ace38d72116e3cdafad9215b1a5d1a33c34
Yaroslav Halchenko <debian@onerussian.com>
2025-03-11 16:44:10 -0400

    Provide some composite tag for arm linux

--- tools/pypi-whl/pdm_build.py
+++ tools/pypi-whl/pdm_build.py
@@ -26,7 +26,10 @@ def get_platform_tag():
 
     # Determine OS and construct the appropriate tag
     if sys.platform.startswith("linux"):
-        platform_tag = f"manylinux_2_17_{arch_tag}"
+        if arch_tag == "arm64":
+            platform_tag = f"manylinux_2_17_{arch_tag}.manylinux2014_{arch_tag}.musllinux_1_1_{arch_tag}"
+        else:
+            platform_tag = f"manylinux_2_17_{arch_tag}"
     elif sys.platform == "darwin":
         if arch_tag == "arm64":
             platform_tag = f"macosx_11_0_{arch_tag}"
